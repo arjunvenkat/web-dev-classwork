5e6180e4c0363ccb129061fd33d03694730b609c
Arjun Venkataswamy <avenkat2@gmail.com>
2013-04-30 13:26:33 -0500

    adds playground 4 data

---
+++ playground_4_announcements_data.rb
@@ -0,0 +1,75 @@
+@announcements = [
+  { author: "Vince Cabansag",
+    title: "Design classroom this week",
+    text: "Just a quick note: The design classroom will be unavailable on Monday, April 29 and Tuesday, April 30 as we're filming interviews in the design classroom for the the next version of The Starter League site. User Experience will still be in the design classroom, but Ruby Playground has been moved to the development classroom." },
+  { author: "Kelsey Mok",
+    title: "Ideathon Ideas",
+    text: "The Ideathon Ideas PDF is now available for download!" },
+  { author: "Arvin Dang",
+    title: "Week 3 Events",
+    text: "Field Trip: Trunk Club
+Thursday, April 18th, 5:30pm
+Join us for a visit to Trunk Club. More info and to RSVP, visit http://fieldtrip.splashthat.com/. PW is ilovetsl. Trunk Club can only take 15 guests, so our RSVP count is capped to the first 15 people to reserve a spot. Email arvin@starterleague.com once the list is full to add yourself to the waitlist.
+
+Student Book Club
+Thursday, April 18th, 7pm – Design Room
+UX students are getting together to to read Don't Make Me Think. They'll be meeting regularly to discuss the book. Join them Thursdays, beginning April 18th.
+
+Characterized
+Thursday, April 18th, 7pm
+Our Visual Design instructor Mig is judging Characterized, a design competition in Chicago. It's a total spectator design sport. Check it out at http://characterized.cutandpaste.com/events/chicago_2013.
+
+Treehouse Sponsored Hackathon
+Saturday, April 20th from 10am to 6pm – Design Room
+We're pairing up in teams and running through as much Treehouse as possible in one day. The winning teams receives a Gold Treehouse Subscription for 1-year, and Treehouse swag. Treehouse is also catering a meal for everyone, so lunch is covered!
+
+Sign up before next weekend:
+1 Month Free Treehouse signup: http://trhou.se/starterleaguehackathon
+$9 Monthly Treehouse plan: http://teamtreehouse.com/signup_code/starterleague" },
+  { author: "Kelsey Mok",
+    title: "Internet Connection at 1871",
+    text: "1871 is a huge facility so we have different networks to connect to in different locations.
+
+Development Classroom
+name: Academy Air
+password: rakedbmigrate
+
+Design Classroom
+name: DarkKnight5G
+password: rakedbmigrate
+
+Anywhere else in 1871
+network: 1871member
+username: codeacademy
+password: 1871ca" },
+  { author: "Arvin Dang",
+    title: "Watch Winter 2013 Starter Night",
+    text: "1871 is a huge facility so we have different networks to connect to in different locations.
+
+Development Classroom
+name: Academy Air
+password: rakedbmigrate
+
+Design Classroom
+name: DarkKnight5G
+password: rakedbmigrate
+
+Anywhere else in 1871
+network: 1871member
+username: codeacademy
+password: 1871ca" },
+
+  { author: "Arvin Dang",
+    title: "Technori Discount",
+    text: "Use promo 'TechnoriLovesStarterLeague' to receive a discount on all Technori pitches. Plus there's a blocked off section for students.
+
+Also, upcoming Technori event: Technori Growth Summit Thursday, April 18th Program: 1-5:30pm / AfterParty: 6:00pm Hear from speakers with proven track records in building businesses using various growth techniques. Http://Growth.Technori.com Use 'starterleague' for $20 off.
+
+Direct link for discount: http://technorigrowth.eventbrite.com/?discount=starterleague" }
+]
+
+
+
+
+
+
